12385893d80dd00dd4a92fa0fea197b9b8cafb6d
Filavandrel <rosina-yana98@mail.ru>
2021-10-19 19:50:34 +0300

    First commit!

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// const unitInfo = [];
+// unitInfo[0] = prompt('What is your name?');
+// unitInfo[1] = prompt('What is your Surname?');
+// document.write (unitInfo[0], " ",unitInfo[1]);
+let incr = 10;
+//console.log(++incr);
+console.log(incr++);
